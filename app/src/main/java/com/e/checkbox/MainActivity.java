ec9dd03451990925cab4fe103ed130cec5296225
adultfishchen <adultfish17640336@gmail.com>
2019-04-07 20:16:44 +0800

    Initial commit

---
+++ app/src/main/java/com/e/checkbox/MainActivity.java
@@ -0,0 +1,24 @@
+package com.e.checkbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CheckBox checkBox=(CheckBox)findViewById(R.id.checkBox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(MainActivity.this,"核取 "+isChecked,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
